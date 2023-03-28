f6afa4af35644f0d719da13b7393f75803317a97
harjitsingh3 <singh.harjit09@gmail.com>
2023-03-28 00:13:04 -0400

    Start make_move method

--- lib/board.rb
+++ lib/board.rb
@@ -5,6 +5,8 @@ class Board
     @cups = Array.new(14) {[:stone, :stone, :stone, :stone]}
     @cups[6] = []
     @cups[13] = []
+    @player1_cups = @cups[0..6]
+    @player2_cups = @cups[7..13]
   end
 
   def place_stones
@@ -21,6 +23,27 @@ class Board
   end
 
   def make_move(start_pos, current_player_name)
+    selected_cup = @cups[start_pos]
+    stones = selected_cup.count 
+    @cups[start_pos] = []
+
+    # if current_player_name == name1 
+    #   (start_pos..13).each do |i|
+    #     if @player1_cups.include?(@cups[i])
+    #       cups[i] << selected_cup.shift 
+    #     end
+    #   end
+    # end
+
+    # if current_player_name == name2 
+    #   (start_pos..13).each do |i|
+    #     if @player2_cups.include?(@cups[i])
+    #       cups[i] << selected_cup.shift 
+    #     end
+    #   end
+    # end    
+
+
   end
 
   def next_turn(ending_cup_idx)
@@ -36,8 +59,21 @@ class Board
   end
 
   def one_side_empty?
+    if (0..5).all? { |idx| cups[idx] = [] } || (7..12).all? { |idx| cups[idx] = [] }
+      return true
+    else
+      return false 
+    end
   end
 
   def winner
+    if cups[6].count == cups[13].count
+      return :draw 
+    end
+    if cups[6].count > cups[13].count
+      return Player.name1
+    else 
+      return Player.name2
+    end
   end
 end
